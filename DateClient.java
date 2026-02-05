ded29e949948ffd92df5f686666308ef83fa30d2
vikram-reddy-y <yvikramreddy222@gmail.com>
2026-02-05 16:02:26 -0600

    Add files via upload

---
+++ DateClient.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.net.*;
+
+public class DateClient {
+private static final String SERVER_HOST = "172.20.10.13";
+    private static final int SERVER_PORT = 5000;           
+    public static void main(String[] args) {
+        try {
+            Socket sock = new Socket(SERVER_HOST, SERVER_PORT);
+
+            InputStream in = sock.getInputStream();
+            BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+
+            String line;
+            while ((line = bin.readLine()) != null) {
+                System.out.println("Server date/time: " + line);
+            }
+
+            sock.close();
+        } catch (IOException ioe) {
+            System.err.println("Client error: " + ioe);
+        }
+    }
+}
